9c5fcc50edbbfd2c9eff9dbd090bdb48a1c1fa90
Alexander Reeder <alexgreeder@outlook.COM>
2022-02-01 18:00:00 -0500

    Removed incorrectly cased folder

--- wordswithbirds.client/src/Timer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Timer = (props) => (
-    <div style={{ background: "#eee", borderRadius: '5px', padding: '0 10px' }}>
-        <p><strong>{props.timer?.secondsRemaining ?? "00:00:00"}</strong></p>
-    </div>
-);
-
-export default Timer;
